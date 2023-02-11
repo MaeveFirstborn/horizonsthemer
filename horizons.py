41ff8ceb4e7555c83e9d74e484fa2d606aba6ada
MaeveFirstborn <sonsofstvenus@gmail.com>
2023-02-10 22:32:11 -0500

    Rebased

--- horizons.py
+++ horizons.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 import os
 import sys
-import glob
+from glob import glob
 import time
 import argparse
 import configparser
 from random import * 
+
+def getImagesContext(inputFolder):
+    _images = glob(f"{inputFolder}*.png") + glob(f"{inputFolder}*.jpg")
+    _length = len(_images)
+    return (_images, _length)
+
+directory = "/home/maeve/wallpapers/"
+images, length = getImagesContext(directory)
+
+os.system(f"wal -i {images[0]}")
 
 
